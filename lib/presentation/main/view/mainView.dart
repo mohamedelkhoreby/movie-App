b5a8ccb8fa6905960a8c74447cc60774be5496ac
mohamedelkhoreby <50024109+mohamedelkhoreby@users.noreply.github.com>
2023-07-31 16:39:57 +0300

    resources (routses & strings ) , main view

---
+++ lib/presentation/main/view/mainView.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class MainView extends StatefulWidget {
+  const MainView({super.key});
+  @override
+  MainViewState createState() => MainViewState();
+}
+class MainViewState extends State<MainView>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
